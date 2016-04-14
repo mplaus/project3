73016e505fc859ebd91c5ce76de7de193f07f2ad
plaus <plaus@live.unc.edu>
2016-04-14 09:19:19 -0400

    Commented Javascript file

--- app.js
+++ app.js
@@ -1,124 +1,3 @@
-/*var margin = {top: 20, right: 20, bottom: 30, left: 40};
-var width = 1400 - margin.left - margin.right;
-var height = 700 - margin.top - margin.bottom;
-
-var svg = d3.select('.container').append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
-    .append('g')
-    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-    // transform: translate(40, 20); in css
-
-var scaleX = d3.scale.ordinal()
-    .rangeRoundBands([0, width], 0.2);
-
-var scaleY = d3.scale.linear()
-    .range([height, 0]);
-    
-var xAxis = d3.svg.axis()
-    .scale(scaleX)
-    .orient('bottom');
-    
-var yAxis = d3.svg.axis()
-    .scale(scaleY)
-    .orient('left')
-    .ticks(15, '%');
-
-d3.json('salary_data.json', function(err, data) {
-    if (err) throw error;
-    
-    data.forEach(function(d){
-    
-    
-    for (i in d) {
-        d[i] = Number(d[i].replace(/[^0-9.]+/g, ""));
-    }
-    //console.log(d);
-  });
-    
-        
-    scaleX.domain(data.map(function(d) { return d.year}));
-    scaleY.domain(data.map(function(d) { return d.average_faculty }));
-        
-        
-        
-    svg.append('g')
-        .attr('class', 'x axis')
-        .attr('transform', 'translate(0, ' + height + ')')
-        .call(xAxis);
-
-    svg.append('g')
-        .attr('class', 'y axis')
-        .call(yAxis);
-        
-    var line = d3.svg.line()
-        .x(function(d) { return x(d.year); })
-        .y(function(d) { return y(d.average_faculty); });
-
-            
-        var line = d3.svg.line()
-            .x(function(d,i) { return x(i); })
-            .y(function(d) { return -1 * y(d); })
-        
-        var vis = d3.select("body")
-            .append("svg:svg")
-            
-         
-        var g = vis.append("svg:g")
-            .attr("transform", "translate(0, 200)");    
-          
-        
-        g.append("svg:line")
-            .attr("x1", x(0))
-            .attr("y1", -1 * y(0))
-            .attr("x2", x(w))
-            .attr("y2", -1 * y(0))
-         
-        g.append("svg:line")
-            .attr("x1", x(0))
-            .attr("y1", -1 * y(0))
-            .attr("x2", x(0))
-            .attr("y2", -1 * y(d3.max(data)))
-        
-        g.selectAll(".xLabel")
-            .data(x.ticks(5))
-            .enter().append("svg:text")
-            .attr("class", "xLabel")
-            .text(String)
-            .attr("x", function(d) { return x(d) })
-            .attr("y", 0)
-            .attr("text-anchor", "middle")
-         
-        g.selectAll(".yLabel")
-            .data(y.ticks(4))
-            .enter().append("svg:text")
-            .attr("class", "yLabel")
-            .text(String)
-            .attr("x", 0)
-            .attr("y", function(d) { return -1 * y(d) })
-            .attr("text-anchor", "right")
-            .attr("dy", 4)
-            
-        g.selectAll(".xTicks")
-            .data(x.ticks(5))
-            .enter().append("svg:line")
-            .attr("class", "xTicks")
-            .attr("x1", function(d) { return x(d); })
-            .attr("y1", -1 * y(0))
-            .attr("x2", function(d) { return x(d); })
-            .attr("y2", -1 * y(-0.3))
-         
-        g.selectAll(".yTicks")
-            .data(y.ticks(4))
-            .enter().append("svg:line")
-            .attr("class", "yTicks")
-            .attr("y1", function(d) { return -1 * y(d); })
-            .attr("x1", x(-0.3))
-            .attr("y2", function(d) { return -1 * y(d); })
-            .attr("x2", x(0))
-
-});*/
-
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
@@ -141,34 +20,49 @@ var yAxis = d3.svg.axis()
 
 var line = d3.svg.line()
     .x(function(d) { return x(d.year); })
-    .y(function(d) { return y(d.average_faculty); });
+    .y(function(d) { return y(d.average_faculty); }); // Line tracking Average Faculty Salary
 
 var line2 = d3.svg.line()
     .x(function(d) { return x(d.year); })
-    .y(function(d) { return y(d.median_faculty); });
+    .y(function(d) { return y(d.median_faculty); }); // Line tracking Median Faculty Salary
     
 var line3 = d3.svg.line()
     .x(function(d) { return x(d.year); })
-    .y(function(d) { return y(d.athletic_director); });
+    .y(function(d) { return y(d.athletic_director); }); // Line tracking Athletic Director Salary
     
+var line4 = d3.svg.line()
+    .x(function(d) { return x(d.year); })
+    .y(function(d) { return y(d.in_state_tuition); }); // Line tracking In state Tuition
+    
+var line5 = d3.svg.line()
+    .x(function(d) { return x(d.year); })
+    .y(function(d) { return y(d.out_state_tuition); }); // Line tracking Out of state tuition
+    
+//setting margins
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+//ajax call for json data
 d3.json("salary_data.json", function(error, data) {
   if (error) throw error;
     console.log("hello")
-    
+
+//reads data as a number    
     data.forEach(function(d) {
         for(var i in d) {
             d[i] = Number(d[i].replace(/[^0-9\.]+/g,""));
         }
     });
+    
+//sets x and y axis
   x.domain(d3.extent(data, function(d) { return +d.year; }));
   y.domain(d3.extent([0, d3.max(data, function(d) { return +d.athletic_director; })]));
 
+  
+//label for x axis
   svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
@@ -180,7 +74,7 @@ d3.json("salary_data.json", function(error, data) {
       .style("text-anchor", "end")
       .text("Year");
       
-
+//label for y axis
   svg.append("g")
       .attr("class", "y axis")
       .call(yAxis)
@@ -191,19 +85,31 @@ d3.json("salary_data.json", function(error, data) {
       .style("text-anchor", "end")
       .text("Price ($)");
 
+//path for first line graph
   svg.append("path")
       .datum(data)
       .attr("class", "line")
       .attr("d", line);
   
-  svg.append("path")      // Add the valueline2 path.
+  svg.append("path")      // Add the line2 path.
         .attr("class", "line")
-        .style("stroke", "green")
+        .style("stroke", "black")
         .attr("d", line2(data));
-  svg.append("path")      // Add the valueline2 path.
+        
+  svg.append("path")      // Add the line3 path.
         .attr("class", "line")
         .style("stroke", "red")
         .attr("d", line3(data));
+ 
+  svg.append("path")      // Add the line4 path.
+        .attr("class", "line")
+        .style("stroke", "blue")
+        .attr("d", line4(data));
+        
+  svg.append("path")      // Add the line5 path.
+        .attr("class", "line")
+        .style("stroke", "green")
+        .attr("d", line5(data));
 });
 
 
