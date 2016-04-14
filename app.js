0989b687faa58f6a3abd0c498de949f5412b95dd
plaus <plaus@live.unc.edu>
2016-04-14 11:18:37 -0400

    Fixed x axis

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-//var formatDate = d3.time.format("%y");
+//make an empty variable to stash the last values into so i can sort the legend
 
 var x = d3.time.scale()
     .range([0, width]);
@@ -38,28 +38,58 @@ var line5 = d3.svg.line()
     .x(function(d) { return x(d.year); })
     .y(function(d) { return y(d.out_state_tuition); }); // Line tracking Out of state tuition
     
+ //Menu selection
+    
 //setting margins
-var svg = d3.select("body").append("svg")
+var svg = d3.select(".container").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+/*    
+//menu change graph
+var menu = d3.select("#menu select")
+    .on("change", change);
+    */
 
 //ajax call for json data
 d3.json("salary_data.json", function(error, data) {
   if (error) throw error;
-    console.log("hello")
+/*  
+function change() {
+  d3.transition()
+      .duration(altKey ? 7500 : 1500)
+      .each(redraw);
+}
+
+// change values of path and then the circles to those of the new series
+    thegraphUpdate.select("path")
+    	.attr("d", function(d, i) {       
+      
+      		//must be a better place to put this, but this works for now
+      		lastvalues[i]=d.values[d.values.length-1].value;         
+        	lastvalues.sort(function (a,b){return b-a});
+      		legendscale.domain(lastvalues);
+      	
+      		return line(d.values); });
+      		*/
 
 //reads data as a number    
     data.forEach(function(d) {
         for(var i in d) {
             d[i] = Number(d[i].replace(/[^0-9\.]+/g,""));
+            if (i === "year") {
+                var tmp = new Date();
+                d[i] = tmp.setYear(d[i]);
+            }
         }
     });
     
+    console.log($("#menu").val());
+    
 //sets x and y axis
   x.domain(d3.extent(data, function(d) { return +d.year; }));
-  y.domain(d3.extent([0, d3.max(data, function(d) { return +d.athletic_director; })]));
+  y.domain(d3.extent([0, d3.max(data, function(d) { return $("#menu").val(); } )]));
 
   
 //label for x axis
@@ -78,7 +108,7 @@ d3.json("salary_data.json", function(error, data) {
   svg.append("g")
       .attr("class", "y axis")
       .call(yAxis)
-    .append("text")
+      .append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 6)
       .attr("dy", ".71em")
@@ -89,7 +119,7 @@ d3.json("salary_data.json", function(error, data) {
   svg.append("path")
       .datum(data)
       .attr("class", "line")
-      .attr("d", line);
+      .attr("d", line)
   
   svg.append("path")      // Add the line2 path.
         .attr("class", "line")
@@ -111,5 +141,6 @@ d3.json("salary_data.json", function(error, data) {
         .style("stroke", "green")
         .attr("d", line5(data));
 });
+
 
 
